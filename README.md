53af6b64cf34c5760e78d11b4cfbc7afc9292b5e
chao-wsl@desktop <gba115566@gmial.com>
2023-10-04 21:30:05 +0800

    Complete spider tool!

--- README.md
+++ README.md
@@ -8,14 +8,14 @@
 - if set_price > new_price
   - 輸出: "物品名稱", "伺服器", "收購/販售", "new 價格"
   - 寫入 db
-  - 通知機器人，nofi寫1
+  - 通知機器人，nofi 寫 1
 - 排序: "高價>低價" or "低價>高價"
 - 爬蟲: 去 db 撈資料開始爬，價格低於特定價格時，寫到 db 去
   - GET(all_items)，然後再一個一個爬
   - 設定的條件
   - 低於某價格時
-    - new_price寫入
-    - nofi寫0
+    - new_price 寫入
+    - nofi 寫 0
 - 間隔查詢: 可以自定義每次爬蟲的間隔（從 db 撈）
 - 通知機器人: telegram，低於特定價格時通知
 - 伺服器合併: 新增 or 合併
@@ -29,7 +29,7 @@
 
 - [x] RO request api
 - [x] Customer(CRUD) api
-- [ ] 爬蟲
+- [x] 爬蟲
 - [ ] 前端
 - [ ] chat-bot
 - [ ] docker
--- routes/get-items.js
+++ routes/get-items.js
@@ -16,8 +16,22 @@ router.get("/", async (req, res) => {
 
   // Loop customers to get items
   for (const customer of customers) {
+    // Get item list of a customer
     const itemList = await getItemList(customer, sort_desc);
     // console.log(itemList);
+
+    // Set price
+    const { set_price } = customer;
+
+    // If price of item list < set_price
+    for (const item of itemList) {
+      if (item.item_price < set_price) {
+        // Perform your action here for items with item_price less than set_price
+        console.log(`Name: ${item.name}, Price: ${item.item_price}`);
+
+        // Send nofi by chat-bot
+      }
+    }
 
     // Set timeout
     await sleep(1000);
