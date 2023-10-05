8c23be90a70861a4b7bf467eb988f75b7a901469
chao-wsl@desktop <gba115566@gmial.com>
2023-10-05 19:41:25 +0800

    beautify TG table

--- client/src/components/TGTable.js
+++ client/src/components/TGTable.js
@@ -1,14 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './TGTable.css'; // Import your CSS file
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./TGTable.css"; // Import your CSS file
 
 function TGTable() {
-  const [data, setData] = useState({ chat_id: '', token: null });
-  const [editedData, setEditedData] = useState({ chat_id: '', token: null });
+  const [data, setData] = useState({ chat_id: "", token: null });
+  const [editedData, setEditedData] = useState({ chat_id: "", token: null });
+
+  const [showTable, setShowTable] = useState(false);
+
+  const handleToggleTable = () => {
+    setShowTable(!showTable);
+  };
 
   useEffect(() => {
     // Fetch data from the API
-    axios.get('http://localhost:3030/chat-id')
+    axios
+      .get("http://localhost:3030/chat-id")
       .then((response) => {
         // Extract the values from the API response
         const { chat_id, token } = response.data;
@@ -16,7 +23,7 @@ function TGTable() {
         setEditedData({ chat_id, token });
       })
       .catch((error) => {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       });
   }, []);
 
@@ -29,40 +36,56 @@ function TGTable() {
 
   const handleSave = () => {
     // Save the edited data to the API
-    axios.post('http://localhost:3030/chat-id', editedData)
+    axios
+      .post("http://localhost:3030/chat-id", editedData)
       .then(() => {
-        alert('Data saved successfully!');
+        alert("Data saved successfully!");
       })
       .catch((error) => {
-        console.error('Error saving data:', error);
+        console.error("Error saving data:", error);
       });
   };
 
   return (
     <div>
-      <h2>Telegram ID/Token</h2>
-      <table className="tg-table">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Token</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td contentEditable={true} onInput={(e) => handleEdit(e.target.textContent, 'chat_id')}>
-              {editedData.chat_id}
-            </td>
-            <td contentEditable={true} onInput={(e) => handleEdit(e.target.textContent, 'token')}>
-              {editedData.token}
-            </td>
-            <td>
-              <button onClick={handleSave}>Save</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <button onClick={handleToggleTable} className="tg-show-button">
+        Telegram Setting
+      </button>
+
+      {showTable && (
+        <div className="tg-container">
+          <h2>Telegram ID/Token</h2>
+          <div className="tg-table">
+            <div className="tg-row">
+              <div className="tg-label">ID:</div>
+              <div
+                className="tg-data"
+                contentEditable={true}
+                onInput={(e) => handleEdit(e.target.textContent, "chat_id")}
+              >
+                {editedData.chat_id}
+              </div>
+            </div>
+
+            <div className="tg-row">
+              <div className="tg-label">Token:</div>
+              <div
+                className="tg-data"
+                contentEditable={true}
+                onInput={(e) => handleEdit(e.target.textContent, "token")}
+              >
+                {editedData.token}
+              </div>
+            </div>
+
+            <div className="tg-button-container">
+              <div className="tg-button" onClick={handleSave}>
+                Save
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
