d0ce5002de9113342748141b5fff7ce927ede915
chao-wsl@lab <gba115566@gmail.com>
2023-10-06 17:55:07 +0800

    refacting

--- client/src/components/PriceChecker.js
+++ client/src/components/PriceChecker.js
@@ -51,8 +51,8 @@ const PriceChecker = () => {
     console.log(`Start checking for every ${timeoutSeconds}(sec)!`);
 
     // Call priceChecking every X seconds
-    // const intervalId = setInterval(priceChecking, intervalDuration);
-    const intervalId = setInterval(priceChecking, 2000);
+    const intervalId = setInterval(priceChecking, intervalDuration);
+    // const intervalId = setInterval(priceChecking, 2000);
     setPriceCheckingIntervalId(intervalId);
   };
 
