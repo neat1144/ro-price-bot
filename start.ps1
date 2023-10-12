be1ce3a46c90f7daf0bef9fc3ddbabb664e247e2
chao-wsl@desktop <gba115566@gmial.com>
2023-10-12 20:03:40 +0800

    add powershell script

---
+++ start.ps1
@@ -0,0 +1,26 @@
+# Navigate to the directory of your Node.js backend
+Set-Location -Path "C:\ro-price-bot-main"
+
+# Check if node_modules directory exists for the backend
+if (-Not (Test-Path "node_modules")) {
+    # Install Node.js dependencies for the backend
+    npm install
+}
+
+# Start the Node.js server (backend)
+Start-Process node app.js
+
+# Open a new PowerShell session for the React app (frontend)
+Start-Process powershell
+
+# Navigate to the directory of your React app
+Set-Location -Path "C:\ro-price-bot-main\client"
+
+# Check if node_modules directory exists for the frontend
+if (-Not (Test-Path "node_modules")) {
+    # Install React dependencies for the frontend
+    npm install
+}
+
+# Start the React app
+npm start
