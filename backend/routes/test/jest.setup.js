f464ad27dc2705133fc549ca45f3362bcd417c4e
chao-wsl@desktop <gba115566@gmial.com>
2023-10-25 11:19:58 +0800

    add global setup for jest

---
+++ backend/routes/test/jest.setup.js
@@ -0,0 +1,16 @@
+// const sqlite3 = require("sqlite3").verbose();
+
+// // Initialize an in-memory SQLite database
+// const db = new sqlite3.Database(":memory:");
+
+// // Close the database connection when Jest is done
+// afterAll((done) => {
+//   db.close((err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     done();
+//   });
+// });
+
+// global.db = db;
