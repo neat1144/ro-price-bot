fc1e7babee42cae248fba4ba47b510acc64beca6
Chaowei (@macbook) <gba115566@gmail.com>
2023-11-16 11:50:29 +0800

    update bot msg format

--- backend/models/chatBot.js
+++ backend/models/chatBot.js
@@ -36,20 +36,29 @@ export const formatMsg = (parent, child) => {
     nofi_time,
     item_name,
     item_CNT,
+    set_refine,
+    set_level,
   } = child;
 
   const chnType = type === 0 ? "販賣" : type === 1 ? "收購" : "未知";
 
-  // Msg by sended
+  // set set_level to eng
+  // 0:"", 1:"D", 2:"C", 3:"B", 4:"A"
+  const levelList = ["", "D階", "C階", "B階", "A階"];
+  const engLevel = levelList[set_level];
+
+  // if set_refine is 0, then set to ""
+  const newRefine = set_refine === 0 ? "" : `+${set_refine}`;
+
   const messageText = `
-  物品名稱: ${item_name}
-  伺服器　: ${svr}
-  設定價格: ${set_price.toLocaleString("en-US")} 
-  ${chnType}價格: ${new_price.toLocaleString("en-US")}
-  數量　　: ${item_CNT}
-  關鍵字　: ${keyword}, 包含(${include}), 排除(${exclude})
-  時間　　: ${nofi_time}
-  `;
+    物品名稱: ${newRefine} ${item_name} ${engLevel}
+    伺服器　: ${svr}
+    設定價格: ${set_price.toLocaleString("en-US")} 
+    ${chnType}價格: ${new_price.toLocaleString("en-US")}
+    數量　　: ${item_CNT}
+    關鍵字　: ${keyword}, 包含(${include}), 排除(${exclude}), (+${set_refine}), (${engLevel})
+    時間　　: ${nofi_time}
+    `;
 
   return messageText;
 };
